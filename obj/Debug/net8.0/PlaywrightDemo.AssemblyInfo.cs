73fb7ead49f4e024fdc965c74b7495e160e31d1b
gepolyz <gepoliz95@yahoo.gr>
2025-05-21 11:58:48 +0300

    playwright.yml file

--- obj/Debug/net8.0/PlaywrightDemo.AssemblyInfo.cs
+++ obj/Debug/net8.0/PlaywrightDemo.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PlaywrightDemo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c7234a03fa0052eb024fb1e967de55dcea4e48ef")]
 [assembly: System.Reflection.AssemblyProductAttribute("PlaywrightDemo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PlaywrightDemo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
